bc4dd35fcce922fd38229401003d1ec9250febfd
kugehouju <kugehouju@gmail.com>
2025-01-21 15:38:04 +0900

    feat: Created the look and feel of the dictionary page.

--- src/pages/dictionary/dictionary.js
+++ src/pages/dictionary/dictionary.js
@@ -1,8 +1,21 @@
-// src/pages/dictionary/dictionary.js
 function Dictionary() {
   return `
-        <h1>This page is Dictionary</h1>
-    `;
+    <div class="search-container">
+      <input type="text" id="search-bar" placeholder="検索" />
+      <button id="search-button">検索</button>
+    </div>
+    <div id="dictionary-content"">
+      <h1>サマリー 【summary】</h1>
+      <ul>
+        <li>まとめ、要約</li>
+        <li>長い文章や大規模なデータなどを集計したり要約したものをサマリー、と言います。</li>
+        <li>会議や議事録やイベントの報告などを手短にまとめた文書のことをサマリーということがある。サマリーを作成する（要約する）ことを「サマライズ」（summarize）というが、日本語の口語では俗に「サマる」という場合がある。</li>
+      </ul>
+    </div>
+    <div id="no-results" style="display: none; color: red;">
+      検索結果がありません。
+    </div>
+  `;
 }
 
 export default Dictionary;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,8 @@
 // src/pages/Home.js
 function HomePage() {
   return `
-    <section>
       <h1>Welcome to My Website</h1>
       <p>This is the homepage.</p>
-    </section>
   `;
 }
 
