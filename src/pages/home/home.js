a1c9d0c9a4ce45d2e5a2df9897b8d20339de9db9
kugehouju <kugehouju@gmail.com>
2025-01-21 16:01:32 +0900

    feat: Created the look and feel of the home.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,33 @@
 // src/pages/Home.js
 function HomePage() {
   return `
-      <h1>Welcome to My Website</h1>
-      <p>This is the homepage.</p>
+<div class="Quiz">
+      <div class="QuizButton">
+        <a href="#">コミュニケーション関連</a>
+        <p>例：ASAP,キャッチアップ,リマインド...</p>
+      </div>
+  
+          <div class="QuizButton">
+        <a href="#">ワークスタイル・業務プロセス関連</a>
+        <p>例：シームレス,スコープ,タイムライン...</p>
+      </div>
+  
+          <div class="QuizButton">
+        <a href="#">プロジェクト管理・進行関連</a>
+        <p>例：アジェンダ,マイルストーン,デリバリー...</p>
+      </div>
+  
+          <div class="QuizButton">
+        <a href="#">技術・システム運用関連</a>
+        <p>例：ハンズオン,デプロイ,モジュール...</p>
+      </div>
+  
+          <div class="QuizButton">
+        <a href="#">ビジネス戦略・マーケティング関連</a>
+        <p>例：KPI,バジェット,セグメンテーション...</p>
+      </div>
+  
+</div>
   `;
 }
 
