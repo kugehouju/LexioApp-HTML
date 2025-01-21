be7b6467eac634778691f76990fa318a8c4088b4
kugehouju <kugehouju@gmail.com>
2025-01-21 16:37:58 +0900

    feat: Create a back-again look and feel

--- src/pages/backagain/backagain.js
+++ src/pages/backagain/backagain.js
@@ -1,8 +1,21 @@
 // src/pages/dictionary/backagain.js
 function Backagain() {
   return `
-        <h1>This page is BackAgain</h1>
-    `;
+  <div class="BackAgainPage">
+    <div class="FailedQuiz">
+        <h1>最近の間違いを直そう。</h1>
+          <div class="solid"></div>
+        <h2>間違い 2個</h2>
+          <div class="FailedList">
+            <p>次の単語に一致するものはどれですか？</p>
+            <p><span>シームレス<span></p>
+          <div class="solid"></div>
+            <p>文章を読み、答えてください。</p>
+          </div>  
+    </div>
+    <button href="#" class="StartBtn">回答する</button>
+  </div>    
+`;
 }
 
 export default Backagain;
